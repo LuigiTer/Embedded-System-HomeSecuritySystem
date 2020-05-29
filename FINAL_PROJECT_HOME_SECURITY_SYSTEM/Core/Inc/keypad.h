8930cbc049148f4697f3400f13409589ff100cb6
giorge1 <g.ammendola4@studenti.unisa.it>
2020-05-29 13:30:20 +0200

    added keyboard configuration and prototypes

---
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/keypad.h
@@ -0,0 +1,70 @@
+/*
+ * keyboard.h
+ *
+ *  Created on: May 28, 2020
+ *      Author: gioam
+ */
+
+#ifndef INC_KEYPAD_H_
+#define INC_KEYPAD_H_
+
+#include "sys/_stdint.h"
+#include "keypad_configuration.h"
+
+/**
+ * @brief  Keypad Keys enumeration
+ */
+typedef enum {
+	KEYPAD_Button_0 = '0', /*!< Button 0 code */
+	KEYPAD_Button_1 = '1', /*!< Button 1 code */
+	KEYPAD_Button_2 = '2', /*!< Button 2 code */
+	KEYPAD_Button_3 = '3', /*!< Button 3 code */
+	KEYPAD_Button_4 = '4', /*!< Button 4 code */
+	KEYPAD_Button_5 = '5', /*!< Button 5 code */
+	KEYPAD_Button_6 = '6', /*!< Button 6 code */
+	KEYPAD_Button_7 = '7', /*!< Button 7 code */
+	KEYPAD_Button_8 = '8', /*!< Button 8 code */
+	KEYPAD_Button_9 = '9', /*!< Button 9 code */
+	KEYPAD_Button_A = 'A', /*!< Button A code */
+	KEYPAD_Button_B = 'B', /*!< Button B code */
+	KEYPAD_Button_C = 'C', /*!< Button C code */
+	KEYPAD_Button_D = 'D', /*!< Button D code */
+	KEYPAD_Button_STAR = '*', /*!< Button STAR code */
+	KEYPAD_Button_HASH = '#', /*!< Button HASH code */
+	KEYPAD_Button_NOT_PRESSED = '\0' /*!< No button pressed */
+} KEYPAD_Button_t;
+
+
+const static KEYPAD_Button_t KEYS[ROWS_N][COLUMNS_N] = {
+		{KEYPAD_Button_1, KEYPAD_Button_2, KEYPAD_Button_3, KEYPAD_Button_A},
+		{KEYPAD_Button_4, KEYPAD_Button_5, KEYPAD_Button_6, KEYPAD_Button_B},
+		{KEYPAD_Button_7, KEYPAD_Button_8, KEYPAD_Button_9, KEYPAD_Button_C},
+		{KEYPAD_Button_STAR, KEYPAD_Button_0, KEYPAD_Button_HASH, KEYPAD_Button_D}
+};
+
+
+
+/* Functions declarations */
+
+/**
+ * @brief  Initializes keypad functionality
+ * @param  None
+ * @retval None
+ */
+void KEYPAD_Init();
+
+/**
+ * @brief  Read the pressed key on the keypad
+ * @param  None
+ * @retval The read value, if present
+ */
+KEYPAD_Button_t KEYPAD_Read(void);
+
+/**
+ * @brief  Updates the pressed key. Should be called only by the interrupt routine
+ * @param  None
+ * @retval None
+ */
+void KEYPAD_Update(uint16_t pin);
+
+#endif /* INC_KEYPAD_H_ */
---
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/keypad_configuration.h
@@ -0,0 +1,40 @@
+/*
+ * keypad_configuration.h
+ *
+ *  Created on: May 28, 2020
+ *      Author: gioam
+ */
+
+#ifndef INC_KEYPAD_CONFIGURATION_H_
+#define INC_KEYPAD_CONFIGURATION_H_
+
+#define COLUMNS_N 		(4)
+#define ROWS_N 			(4)
+
+/* Column 1 default */
+#define COLUMN_1_PORT  	GPIOC
+#define COLUMN_1_PIN  	GPIO_PIN_0
+/* Column 2 default */
+#define COLUMN_2_PORT  	GPIOC
+#define COLUMN_2_PIN  	GPIO_PIN_1
+/* Column 3 default */
+#define COLUMN_3_PORT  	GPIOC
+#define COLUMN_3_PIN  	GPIO_PIN_2
+/* Column 4 default */
+#define COLUMN_4_PORT  	GPIOC
+#define COLUMN_4_PIN  	GPIO_PIN_3
+
+/* Row 1 default */
+#define ROW_1_PORT  	GPIOB
+#define ROW_1_PIN  		GPIO_PIN_12
+/* Row 2 default */
+#define ROW_2_PORT  	GPIOB
+#define ROW_2_PIN  		GPIO_PIN_13
+/* Row 3 default */
+#define ROW_3_PORT  	GPIOB
+#define ROW_3_PIN 	 	GPIO_PIN_14
+/* Row 4 default */
+#define ROW_4_PORT  	GPIOB
+#define ROW_4_PIN  		GPIO_PIN_15
+
+#endif /* INC_KEYPAD_CONFIGURATION_H_ */
