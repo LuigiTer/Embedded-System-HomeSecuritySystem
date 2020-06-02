4599c1857bb6c6b7a87578ded4a7d2e2225ea69a
VincenzoPetrone <v.petrone6@studenti.unisa.it>
2020-06-02 23:54:41 +0200

    console module documentation completed

--- FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/console.h
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/console.h
@@ -1,63 +1,107 @@
+/*
+ * This module contains methods to handle with the console, representing it with a singleton.
+ * To instantiate a console, just call consoleInit() with the UART interface you want to use to communicate.
+ * To print messages, just call the proper methods, without specifying again the UART interface.
+ */
+
 #ifndef INC_CONSOLE_H_
 #define INC_CONSOLE_H_
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "stm32f4xx_hal.h"
 #include "serial.h"
 #include "bool.h"
 #include "utils.h"
 
+#define ESCAPE_SEQUENCE_CLEAR_CONSOLE ("\033[2J")
+#define ESCAPE_SEQUENCE_TOPLEFT_CURSOR ("\033[0;0H")
 
+/*
+ * @brief	This struct represents the console singleton,
+ * 			encapsulating the UART interface used for communication.
+ * @param	huart	pointer to the UART_HandleTypeDef structure
+ * 					representing the UART interface used for communication
+ * @param	ready	a boolean value set to TRUE if the console is ready to use, FALSE otherwise
+ */
 typedef struct {
 	UART_HandleTypeDef *huart;
 	bool ready;
 } TConsole;
 
-
 /*
- * Creates the console singleton.
- * This function must be called before calling every other function in this module.
+ * @brief	Creates the console singleton.
+ * 			This function must be called before calling every other function in this module.
+ * @param	huart	pointer to the UART_HandleTypeDef structure
+ * 					representing the UART interface that will be used for communication
  */
 void consoleInit(UART_HandleTypeDef *huart);
 
-
 /*
- * Returns the singleton console instance.
- * If the instance has not been initialized yet and huart is not NULL,
- * then it will be initialized with huart and so huart itself will be returned.
- * If the instance has not been initialized yet and huart is NULL, then the function will raise an error.
- * If the instance has already been initialized,
- * the parameter huart will be uneffective and the previous instance will be returned instead.
+ * @brief	Returns the singleton console instance.
+ * 			If the instance has not been initialized yet and huart is not NULL,
+ * 				then it will be initialized with huart itself.
+ * 			If the instance has not been initialized yet and huart is NULL,
+ * 				then the function will raise an error.
+ * 			If the instance has already been initialized, than the parameter huart will be uneffective
+ * 				and the previous instance will be returned instead.
+ * @param	huart	pointer to the UART_HandleTypeDef structure
  */
 TConsole* getConsole(UART_HandleTypeDef *huart);
 
 /*
- * Clears the console
+ * @brief	Clears the console
  */
 void clearConsole();
 
+/*
+ * @brief	Waits unitl the console is ready to be used
+ */
 void freeConsole();
 
 /*
- * Prints a char message on the console
+ * @brief	Prints a string on the console.
+ * 			It wraps the particular mode for transmission.
+ * @param	message		string to print
+ */
+static void printMessage(const char *message) {
+	HAL_UART_Transmit_DMA(getConsole(NULL)->huart, (uint8_t*) message,
+			strlen(message));
+}
+
+/*
+ * @brief	Prints a string on the console, waiting if it is not ready to use
+ * @param	message		string to print
  */
 void printOnConsole(const char *message);
 
 /*
- * Prints an integer on the console
+ * @brief	Prints an integer on the console, waiting if it is not ready to use
+ * #param	n	number to print
  */
 void printIntOnConsole(const uint16_t n);
 
-
+/*
+ * @brief	Trasmits the content of a data buffer on the console
+ * @param	data	buffer containing the data to transmit
+ * @param	n		length of the data to transmit
+ */
 void transmit(uint8_t *data, uint8_t n);
 
+/*
+ * @brief	Receives data from the console and stores them in a buffer
+ * @param	data	buffer the received data will be stored in
+ * @param	n		length of the data to receive
+ */
 void receive(uint8_t *data, uint8_t n);
 
 /*
- * Echoes a n-byte-long message on the UART interface, assigning the result to str.
+ * @brief	Echoes a message on the console, storing it in a string.
+ * @param	n		number of characters to trasmit
+ * @param	str		string the data will be stored in
  */
-void echo(UART_HandleTypeDef *huart, const uint8_t n, char *str);
+void echo(const uint8_t n, char *str);
 
 #endif /* INC_CONSOLE_H_ */
--- FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Src/console.c
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Src/console.c
@@ -1,65 +1,77 @@
+/*
+ * This module contains methods to handle with the console, representing it with a singleton.
+ * To instantiate a console, just call consoleInit() with the UART interface you want to use to communicate.
+ * To print messages, just call the proper methods, without specifying again the UART interface.
+ */
+
 #include "console.h"
 
 /*
- * Creates the console singleton.
- * This function must be called before calling every other function in this module.
+ * @brief	Creates the console singleton.
+ * 			This function must be called before calling every other function in this module.
+ * @param	huart	pointer to the UART_HandleTypeDef structure
+ * 					representing the UART interface that will be used for communication
  */
 void consoleInit(UART_HandleTypeDef *huart) {
 	getConsole(huart);
 }
 
 /*
- * Returns the singleton console instance.
- * If the instance has not been initialized yet and huart is not NULL,
- * then it will be initialized with huart and so the instance itself will be returned.
- * If the instance has not been initialized yet and huart is NULL, then the function will raise an error.
- * If the instance has already been initialized,
- * the parameter huart will be ineffective and the previous instance will be returned instead.
+ * @brief	Returns the singleton console instance.
+ * 			If the instance has not been initialized yet and huart is not NULL,
+ * 				then it will be initialized with huart itself.
+ * 			If the instance has not been initialized yet and huart is NULL,
+ * 				then the function will raise an error.
+ * 			If the instance has already been initialized, than the parameter huart will be uneffective
+ * 				and the previous instance will be returned instead.
+ * @param	huart	pointer to the UART_HandleTypeDef structure
  */
 TConsole* getConsole(UART_HandleTypeDef *huart) {
-	static TConsole *instance = NULL;
+	static TConsole *console = NULL;
 
-	if (huart == NULL && instance == NULL) {
+	if (huart == NULL && console == NULL) {
 		exit(EXIT_FAILURE);
 	}
 
-	if (instance == NULL) {
-		instance = malloc(sizeof(*instance));
-		instance->huart = huart;
-		instance->ready = TRUE;
+	if (console == NULL) {
+		console = malloc(sizeof(*console));
+		console->huart = huart;
+		console->ready = TRUE;
 	}
 
-	return instance;
+	return console;
 }
 
+/*
+ * @brief	Waits unitl the console is ready to be used
+ */
 void freeConsole() {
 	while (!getConsole(NULL)->ready) {
 		HAL_Delay(1);
 	}
 }
 
-
 /*
- * Prints a char message on the console
+ * @brief	Prints a string on the console, waiting if it is not ready to use
+ * @param	message		string to print
  */
 void printOnConsole(const char *message) {
-	TConsole *console = getConsole(NULL);
-	UART_HandleTypeDef *huart = console->huart;
 	freeConsole();
-	console->ready = FALSE;
-	printMessage(huart, message);
+	getConsole(NULL)->ready = FALSE;
+	printMessage(message);
 }
 
 /*
- * Clears the console
+ * @brief	Clears the console
  */
 void clearConsole() {
-	printOnConsole(CLEAR_CONSOLE_ESCAPE_SEQUENCE);
-	printOnConsole(TOPLEFT_CURSOR_ESCAPE_SEQUENCE);
+	printOnConsole(ESCAPE_SEQUENCE_CLEAR_CONSOLE);
+	printOnConsole(ESCAPE_SEQUENCE_TOPLEFT_CURSOR);
 }
 
 /*
- * Prints an integer on the console
+ * @brief	Prints an integer on the console, waiting if it is not ready to use
+ * #param	n	number to print
  */
 void printIntOnConsole(const uint16_t n) {
 	char str[digitsOf(n)];
@@ -67,6 +79,11 @@ void printIntOnConsole(const uint16_t n) {
 	printOnConsole(str);
 }
 
+/*
+ * @brief	Trasmits the content of a data buffer on the console
+ * @param	data	buffer containing the data to transmit
+ * @param	n		length of the data to transmit
+ */
 void transmit(uint8_t *data, uint8_t n) {
 	TConsole *console = getConsole(NULL);
 	UART_HandleTypeDef *huart = console->huart;
@@ -75,6 +92,11 @@ void transmit(uint8_t *data, uint8_t n) {
 	HAL_UART_Transmit_DMA(huart, data, n);
 }
 
+/*
+ * @brief	Receives data from the console and stores them in a buffer
+ * @param	data	buffer the received data will be stored in
+ * @param	n		length of the data to receive
+ */
 void receive(uint8_t *data, uint8_t n) {
 	TConsole *console = getConsole(NULL);
 	UART_HandleTypeDef *huart = console->huart;
@@ -85,17 +107,15 @@ void receive(uint8_t *data, uint8_t n) {
 }
 
 /*
- * Echoes a n-byte-long message on the UART interface, assigning the result to str.
+ * @brief	Echoes a message on the console, storing it in a string.
+ * @param	n		number of characters to trasmit
+ * @param	str		string the data will be stored in
  */
-void echo(UART_HandleTypeDef *huart, const uint8_t n, char *str) {
+void echo(const uint8_t n, char *str) {
 	uint8_t buf[1];
 	for (uint8_t i = 0; i < n; i++) {
-		// HAL_UART_Receive(huart, (uint8_t*) buf, 1, HAL_MAX_DELAY);
-		// HAL_UART_Receive_DMA(huart, (uint8_t*) buf, 1);
 		receive(buf, 1);
 		str[i] = buf[0];
-		// HAL_UART_Transmit(huart, (uint8_t*) buf, 1, HAL_MAX_DELAY);
-		// HAL_UART_Transmit_DMA(huart, (uint8_t*) buf, 1);
 		transmit(buf, 1);
 	}
 }
