48d12661d510c8a6ee9157f88ebec3c5ff2f6847
VincenzoPetrone <v.petrone6@studenti.unisa.it>
2020-06-08 16:45:29 +0200

    buzzer module documentation changed

--- FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/buzzer.h
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Inc/buzzer.h
@@ -2,7 +2,11 @@
  * This module contains methods to handle with a buzzer, represented with a structure holding:
  * 		a pointer to the TIM_HandleTypeDef structure representing the timer generating the PWM signal
  * 			that controls the sound of the buzzer
+ * 		an integer representing the timer channel holding the PWM signal
  * 		the current duty cycle in [0, 1[
+ * 		the pulse corresponding to the current duty cycle, depending on the timer period
+ * 		the pulse corresponding to the previously set duty cycle
+ * 		a boolean variable to make the buzzer emit just a single sound instead of a periodic one
  */
 
 #ifndef INC_BUZZER_H_
@@ -16,16 +20,25 @@
 #include "bool.h"
 #include "tim.h"
 
+/* Period of the buzzer timer */
 #define BUZZER_MAX_PULSE		(1000)
+
+/* The shortest sound the buzzer can produce */
 #define BUZZER_BEEP				(0.1 * BUZZER_MAX_PULSE)
+
+/* A short sound the buzzer can produce */
 #define BUZZER_SHORT_PULSE		(0.2 * BUZZER_MAX_PULSE)
+
+/* A medium-length sound the buzzer can produce */
 #define BUZZER_MEDIUM_PULSE		(0.6 * BUZZER_MAX_PULSE)
+
+/* The longest sound the buzzer can produce */
 #define BUZZER_LONG_PULSE		(0.9 * BUZZER_MAX_PULSE)
 
-// the duty cycle is in [0, 1[ so it's stored in a float variable
+/* the duty cycle is in [0, 1[ so it's stored in a float variable */
 typedef float TDutyCycle;
 
-// the integer value of the duty cycle is in [0, MAX_VALUE[ so it's stored in an integer variable
+/* the integer value of the duty cycle is in [0, BUZZER_MAX_PULSE[ so it's stored in an integer variable */
 typedef uint32_t TPulse;
 
 /*
@@ -33,14 +46,20 @@ typedef uint32_t TPulse;
  * 			encapsulating the timer interface used to generate the PWM signal that control the buzzer.
  * @param	htim				pointer to the TIM_HandleTypeDef structure representing the timer interface
  * 								used to generate the PWM signal that control the buzzer
- * @param	current_duty_cycle	the current duty cycle
+ * @param	Channel				integer representing the timer cannel holding the PWM signal
+ * @param	duty_cycle			the current duty cycle
+ * @param	previous_pulse		the pulse corresponding to the previously set duty cycle
+ * @param	pulse				the pulse corresponding to the current duty cycle
+ * @param	single_pulse_mode	a boolean variable.
+ * 								if TRUE, the buzzer will emit just a single sound;
+ * 								otherwise, the buzzer will emit a period sound
  */
 typedef struct {
 	TIM_HandleTypeDef *htim;
 	uint32_t Channel;
-	TPulse pulse;
-	TPulse previous_pulse;
 	TDutyCycle duty_cycle;
+	TPulse previous_pulse;
+	TPulse pulse;
 	bool single_pulse_mode;
 } TBuzzer;
 
@@ -55,7 +74,7 @@ typedef struct {
 TBuzzer* buzzer_init(TIM_HandleTypeDef *htim, uint32_t Channel);
 
 /*
- * @fn		TDutyCycle get_duty_cycle(TBuzzer *buzzer)
+ * @fn		TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer)
  * @brief	Returns the duty cycle of a buzzer
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
  * @retval	current duty cycle of the buzzer
@@ -63,58 +82,154 @@ TBuzzer* buzzer_init(TIM_HandleTypeDef *htim, uint32_t Channel);
 TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer);
 
 /*
- * @fn		TValue get_duty_cycle_value(TBuzzer *buzzer)
- * @brief	Returns the integer duty cycle value of a buzzer
+ * @fn		TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer)
+ * @brief	Returns the duty cycle of a buzzer
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @retval	current integer duty cycle value of the buzzer
+ * @retval	current duty cycle of the buzzer
  */
 TPulse buzzer_get_pulse(TBuzzer *buzzer);
 
 
 /*
- * @fn		void set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle)
- * @brief	Sets the duty cycle of a buzzer to a specified value in [0, 1[
+ * @fn		void buzzer_set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle)
+ * @brief	Sets the duty cycle of a buzzer to a specified value in [0, 1[,
+ * 			without playing any sound
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @param	dutyCycle	duty cycle to set
+ * @param	duty_cycle	duty cycle to set
  */
 void buzzer_set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle);
 
 /*
- * @fn		void set_duty_cycle_to_value(TBuzzer *buzzer, const TValue value)
- * @brief	Sets the duty cycle of a buzzer to a value corresponding to an integer value in [0, MAX_VALUE[
- * 			The conversion formula is dutyCycle = value / MAX_VALUE
- * 			For example, if value is 100, then the duty cycle will be 100 / 255 = 0,3922
+ * @fn		void buzzer_set_pulse(TBuzzer *buzzer, const TPulse pulse)
+ * @brief	Sets the duty cycle of a buzzer to a value corresponding
+ * 			to an integer value in [0, BUZZER_MAX_PULSE[, without playing any sound.
+ * 			The conversion formula is duty_cycle = pulse / BUZZER_MAX_PULSE
+ * 			For example, if pulse is 100 and BUZZER_MAX_PULSE is 255,
+ * 			then the duty cycle will be 100 / 255 = 0.3922
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @param	value		integer duty cycle value to set
+ * @param	pulse		duty cycle pulse to set
  */
 void buzzer_set_pulse(TBuzzer *buzzer, const TPulse pulse);
 
+/*
+ * @fn		void buzzer_play_duty_cycle(TBuzzer *buzzer, TDutyCycle duty_cycle)
+ * @brief	Makes a buzzer emit a sound for a certain ratio of the timer period.
+ * 			For example, if the period is 1 second and duty_cycle is 0.4,
+ * 			then the buzzer will emit a sound for 400 ms.
+ * @param	buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param	duty_cycle		duty cycle in [0, 1[ to set
+ */
 void buzzer_play_duty_cycle(TBuzzer *buzzer, TDutyCycle duty_cycle);
 
+/*
+ * @fn		void buzzer_play_pulse(TBuzzer *buzzer, TPulse pulse)
+ * @brief	Makes a buzzer emit a sound with a certain pulse.
+ * 			For example, if is 400, then the buzzer will emit a sound for 400 ms.
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ * @param	pulse		duty cycle pulse in [0, BUZZER_MAX_PULSE[ to set
+ */
 void buzzer_play_pulse(TBuzzer *buzzer, TPulse pulse);
 
+/*
+ * @fn		void buzzer_play_beep(TBuzzer *buzzer)
+ * @brief	Makes a buzzer emit the shortest sound it can produce: it is not periodic
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ */
 void buzzer_play_beep(TBuzzer *buzzer);
 
+/*
+ * @fn		void buzzer_stop(TBuzzer *buzzer)
+ * @brief	Stops a buzzer, making it silent and stopping the PWM signal generation
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ */
 void buzzer_stop(TBuzzer *buzzer);
 
-void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse);
-
-void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse);
-
-void buzzer_change_pulse(TBuzzer *buzzer, uint16_t pulse);
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_BEEP
+ * @retval	BUZZER_BEEP as an integer
+ */
+TPulse buzzer_beep();
 
 /*
- * @fn		static TValue get_value_of_duty_cycle(const TDutyCycle duty_cycle)
- * @brief	Converts a duty cycle float value in [0,1[ to the corresponding interger value in [0, MAX_VALUE[
+ * @fn		TPulse buzzer_short_pulse()
+ * @brief	Returns the pulse value of BUZZER_SHORT_PULSE
+ * @retval	BUZZER_SHORT_PULSE as an integer
+ */
+TPulse buzzer_short_pulse();
+
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_MEDIUM_PULSE
+ * @retval	BUZZER_MEDIUM_PULSE as an integer
+ */
+TPulse buzzer_medium_pulse();
+
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_LONG_PULSE
+ * @retval	BUZZER_LONG_PULSE as an integer
+ */
+TPulse buzzer_long_pulse();
+
+/*
+ * @fn		void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse)
+ * @brief	Makes a buzzer emit a longer sound, depending on the previous pulse.
+ * 			This function is effective only if pulse is in {0, BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}
+ * 			and next_pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}.
+ * 			In this case, if the buzzer is not emitting any sound, the next_pulse sound will be emitted;
+ * 			otherwise, the buzzer will emit the longest sound it can produce
+ * @param	buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param	next_pulse		desidered duty cycle pulse in {0, BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE} to set
+ */
+void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse);
+
+/*
+ * @fn		void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse)
+ * @brief	Makes a buzzer emit a shorter sound, depending on the previous pulse. This function is effective
+ * 			only if pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE, BUZZER_LONG_PULSE}
+ * 			and previous_pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}.
+ * 			In this case, if the buzzer is not emitting the longest sound it can produce, it will be stopped;
+ * 			otherwise, the buzzer will emit a shorter sound
+ * @param buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param previous_pulse	duty cycle pulse that will be stopped
+ */
+void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse);
+
+/*
+ * @fn		static void buzzer_change_pulse(TBuzzer *buzzer, uint16_t pulse)
+ * @brief	Re-initializes the timer channel holding the PWM signal, setting a specified pulse,
+ * 			without playing any sound
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ * @param	pulse		duty cycle pulse in [0, BUZZER_MAX_PULSE[ to set
+ */
+static void buzzer_change_pulse(TBuzzer *buzzer, uint16_t pulse) {
+	TIM_OC_InitTypeDef sConfigOC = { 0 };
+	TIM_HandleTypeDef *htim = buzzer->htim;
+	uint32_t Channel = buzzer->Channel;
+
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = pulse;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	if (HAL_TIM_PWM_ConfigChannel(htim, &sConfigOC, Channel) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_TIM_MspPostInit(htim);
+}
+
+/*
+ * @fn		static TPulse get_pulse_of_duty_cycle(const TDutyCycle duty_cycle)
+ * @brief	Converts a duty cycle float value in [0,1[ to the corresponding pulse value in [0, BUZZER_MAX_PULSE[
  * @param	dutyCycle	duty cycle value to convert
- * @retval	integer value corresponding to duty_cycle
+ * @retval	pulse value corresponding to duty_cycle
  */
 static TPulse get_pulse_of_duty_cycle(const TDutyCycle duty_cycle) {
 	return (TPulse) map(duty_cycle, 0.0, 1.0, 0, BUZZER_MAX_PULSE);
 }
 
 /*
- * @fn		static TDutyCycle get_duty_cycle_of_value(const TValue value)
+ * @fn		static TDutyCycle get_duty_cycle_of_pulse(const TPulse pulse)
  * @brief	Converts a duty cycle integer value in [0, MAX_VALUE[ to the corresponding float value in [0,1[
  * @param	value	integer duty cycle value to convert
  * @retval	float duty cycle value corresponding to value
@@ -122,14 +237,6 @@ static TPulse get_pulse_of_duty_cycle(const TDutyCycle duty_cycle) {
 static TDutyCycle get_duty_cycle_of_pulse(const TPulse pulse) {
 	return map(pulse, 0, BUZZER_MAX_PULSE, 0.0, 1.0);
 }
-
-TPulse buzzer_beep();
-
-TPulse buzzer_short_pulse();
-
-TPulse buzzer_medium_pulse();
-
-TPulse buzzer_long_pulse();
 
 #endif /* INC_BUZZER_H_ */
 
--- FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Src/buzzer.c
+++ FINAL_PROJECT_HOME_SECURITY_SYSTEM/Core/Src/buzzer.c
@@ -2,11 +2,16 @@
  * This module contains methods to handle with a buzzer, represented with a structure holding:
  * 		a pointer to the TIM_HandleTypeDef structure representing the timer generating the PWM signal
  * 			that controls the sound of the buzzer
+ * 		an integer representing the timer channel holding the PWM signal
  * 		the current duty cycle in [0, 1[
+ * 		the pulse corresponding to the current duty cycle, depending on the timer period
+ * 		the pulse corresponding to the previously set duty cycle
+ * 		a boolean variable to make the buzzer emit just a single sound instead of a periodic one
  */
 
 #include "buzzer.h"
 
+/* buzzer used in the main program */
 extern TBuzzer *buzzer;
 
 /*
@@ -30,7 +35,7 @@ TBuzzer* buzzer_init(TIM_HandleTypeDef *htim, uint32_t Channel) {
 }
 
 /*
- * @fn		TDutyCycle get_duty_cycle(TBuzzer *buzzer)
+ * @fn		TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer)
  * @brief	Returns the duty cycle of a buzzer
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
  * @retval	current duty cycle of the buzzer
@@ -40,20 +45,21 @@ TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer) {
 }
 
 /*
- * @fn		TValue get_duty_cycle_value(TBuzzer *buzzer)
- * @brief	Returns the integer duty cycle value of a buzzer
+ * @fn		TDutyCycle buzzer_get_duty_cycle(TBuzzer *buzzer)
+ * @brief	Returns the duty cycle of a buzzer
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @retval	current integer duty cycle value of the buzzer
+ * @retval	current duty cycle of the buzzer
  */
 TPulse buzzer_get_pulse(TBuzzer *buzzer) {
 	return buzzer->pulse;
 }
 
 /*
- * @fn		void set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle)
- * @brief	Sets the duty cycle of a buzzer to a specified value in [0, 1[
+ * @fn		void buzzer_set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle)
+ * @brief	Sets the duty cycle of a buzzer to a specified value in [0, 1[,
+ * 			without playing any sound
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @param	dutyCycle	duty cycle to set
+ * @param	duty_cycle	duty cycle to set
  */
 void buzzer_set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle) {
 	assert(0 <= duty_cycle && duty_cycle < 1);
@@ -66,12 +72,14 @@ void buzzer_set_duty_cycle(TBuzzer *buzzer, const TDutyCycle duty_cycle) {
 }
 
 /*
- * @fn		void set_duty_cycle_to_value(TBuzzer *buzzer, const TValue value)
- * @brief	Sets the duty cycle of a buzzer to a value corresponding to an integer value in [0, MAX_VALUE[
- * 			The conversion formula is dutyCycle = value / MAX_VALUE
- * 			For example, if value is 100, then the duty cycle will be 100 / 255 = 0,3922
+ * @fn		void buzzer_set_pulse(TBuzzer *buzzer, const TPulse pulse)
+ * @brief	Sets the duty cycle of a buzzer to a value corresponding
+ * 			to an integer value in [0, BUZZER_MAX_PULSE[, without playing any sound.
+ * 			The conversion formula is duty_cycle = pulse / BUZZER_MAX_PULSE
+ * 			For example, if pulse is 100 and BUZZER_MAX_PULSE is 255,
+ * 			then the duty cycle will be 100 / 255 = 0.3922
  * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
- * @param	value		integer duty cycle value to set
+ * @param	pulse		duty cycle pulse to set
  */
 void buzzer_set_pulse(TBuzzer *buzzer, const TPulse pulse) {
 	assert(0 <= pulse && pulse < BUZZER_MAX_PULSE);
@@ -82,28 +90,99 @@ void buzzer_set_pulse(TBuzzer *buzzer, const TPulse pulse) {
 	buzzer->duty_cycle = get_duty_cycle_of_pulse(pulse);
 }
 
+/*
+ * @fn		void buzzer_play_duty_cycle(TBuzzer *buzzer, TDutyCycle duty_cycle)
+ * @brief	Makes a buzzer emit a sound for a certain ratio of the timer period.
+ * 			For example, if the period is 1 second and duty_cycle is 0.4,
+ * 			then the buzzer will emit a sound for 400 ms.
+ * @param	buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param	duty_cycle		duty cycle in [0, 1[ to set
+ */
 void buzzer_play_duty_cycle(TBuzzer *buzzer, TDutyCycle duty_cycle) {
 	buzzer_set_duty_cycle(buzzer, duty_cycle);
 	HAL_TIM_PWM_Start_IT(buzzer->htim, buzzer->Channel);
 }
 
+/*
+ * @fn		void buzzer_play_pulse(TBuzzer *buzzer, TPulse pulse)
+ * @brief	Makes a buzzer emit a sound with a certain pulse.
+ * 			For example, if is 400, then the buzzer will emit a sound for 400 ms.
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ * @param	pulse		duty cycle pulse in [0, BUZZER_MAX_PULSE[ to set
+ */
 void buzzer_play_pulse(TBuzzer *buzzer, TPulse pulse) {
 	buzzer_set_pulse(buzzer, pulse);
 	HAL_TIM_PWM_Start_IT(buzzer->htim, buzzer->Channel);
 }
 
+/*
+ * @fn		void buzzer_play_beep(TBuzzer *buzzer)
+ * @brief	Makes a buzzer emit the shortest sound it can produce: it is not periodic
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ */
 void buzzer_play_beep(TBuzzer *buzzer) {
 	HAL_TIM_PWM_Stop_IT(buzzer->htim, buzzer->Channel);
 	buzzer->single_pulse_mode = TRUE;
 	buzzer_play_pulse(buzzer, buzzer_beep());
 }
 
+/*
+ * @fn		void buzzer_stop(TBuzzer *buzzer)
+ * @brief	Stops a buzzer, making it silent and stopping the PWM signal generation
+ * @param	buzzer		pointer to the TBuzzer structure representing the buzzer
+ */
 void buzzer_stop(TBuzzer *buzzer) {
 	buzzer_set_pulse(buzzer, 0);
 	HAL_TIM_PWM_Stop_IT(buzzer->htim, buzzer->Channel);
 	buzzer->single_pulse_mode = FALSE;
 }
 
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_BEEP
+ * @retval	BUZZER_BEEP as an integer
+ */
+TPulse buzzer_beep() {
+	return (TPulse) max(1, BUZZER_BEEP);
+}
+
+/*
+ * @fn		TPulse buzzer_short_pulse()
+ * @brief	Returns the pulse value of BUZZER_SHORT_PULSE
+ * @retval	BUZZER_SHORT_PULSE as an integer
+ */
+TPulse buzzer_short_pulse() {
+	return (TPulse) max(buzzer_beep(), BUZZER_SHORT_PULSE);
+}
+
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_MEDIUM_PULSE
+ * @retval	BUZZER_MEDIUM_PULSE as an integer
+ */
+TPulse buzzer_medium_pulse() {
+	return (TPulse) max(buzzer_beep(), BUZZER_MEDIUM_PULSE);
+}
+
+/*
+ * @fn		TPulse buzzer_beep()
+ * @brief	Returns the pulse value of BUZZER_LONG_PULSE
+ * @retval	BUZZER_LONG_PULSE as an integer
+ */
+TPulse buzzer_long_pulse() {
+	return (TPulse) max(buzzer_beep(), BUZZER_LONG_PULSE);
+}
+
+/*
+ * @fn		void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse)
+ * @brief	Makes a buzzer emit a longer sound, depending on the previous pulse.
+ * 			This function is effective only if pulse is in {0, BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}
+ * 			and next_pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}.
+ * 			In this case, if the buzzer is not emitting any sound, the next_pulse sound will be emitted;
+ * 			otherwise, the buzzer will emit the longest sound it can produce
+ * @param	buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param	next_pulse		desidered duty cycle pulse in {0, BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE} to set
+ */
 void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse) {
 	TPulse current_pulse = buzzer->pulse;
 
@@ -120,6 +199,16 @@ void buzzer_increase_pulse(TBuzzer *buzzer, TPulse next_pulse) {
 	}
 }
 
+/*
+ * @fn		void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse)
+ * @brief	Makes a buzzer emit a shorter sound, depending on the previous pulse. This function is effective
+ * 			only if pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE, BUZZER_LONG_PULSE}
+ * 			and previous_pulse is in {BUZZER_SHORT_PULSE, BUZZER_MEDIUM_PULSE}.
+ * 			In this case, if the buzzer is not emitting the longest sound it can produce, it will be stopped;
+ * 			otherwise, the buzzer will emit a shorter sound
+ * @param buzzer			pointer to the TBuzzer structure representing the buzzer
+ * @param previous_pulse	duty cycle pulse that will be stopped
+ */
 void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse) {
 	TPulse current_pulse = buzzer->pulse;
 
@@ -141,37 +230,11 @@ void buzzer_decrease_pulse(TBuzzer *buzzer, TPulse previous_pulse) {
 	}
 }
 
-TPulse buzzer_beep() {
-	return (TPulse) max(1, BUZZER_BEEP);
-}
-
-TPulse buzzer_short_pulse() {
-	return (TPulse) max(buzzer_beep(), BUZZER_SHORT_PULSE);
-}
-
-TPulse buzzer_medium_pulse() {
-	return (TPulse) max(buzzer_beep(), BUZZER_MEDIUM_PULSE);
-}
-
-TPulse buzzer_long_pulse() {
-	return (TPulse) max(buzzer_beep(), BUZZER_LONG_PULSE);
-}
-
-void buzzer_change_pulse(TBuzzer *buzzer, uint16_t pulse) {
-	TIM_OC_InitTypeDef sConfigOC = { 0 };
-	TIM_HandleTypeDef *htim = buzzer->htim;
-	uint32_t Channel = buzzer->Channel;
-
-	sConfigOC.OCMode = TIM_OCMODE_PWM1;
-	sConfigOC.Pulse = pulse;
-	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-	if (HAL_TIM_PWM_ConfigChannel(htim, &sConfigOC, Channel) != HAL_OK) {
-		Error_Handler();
-	}
-	HAL_TIM_MspPostInit(htim);
-}
-
+/*
+ * This callback is called every time a pulse is over.
+ * If the buzzer is set to play just a single sound (beep),
+ * the previous pulse will be set again after the beep is played
+ */
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance == TIM3) {
 		if (buzzer->single_pulse_mode) {
